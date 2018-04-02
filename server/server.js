988396aa2d2cad03720375ada444ec154da77e60
richardtarnoczi <ricsi960413@gmail.com>
2018-04-02 22:08:17 +0200

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -15,10 +15,11 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     //console.log(req.body);
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
 
     todo.save().then((doc) => {
@@ -28,22 +29,27 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({
+        _creator:req.user._id
+    }).then((todos) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     });
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
       }
     
-      Todo.findById(id).then((todo) => {
+      Todo.findOne({
+          _id: id,
+          _creator: req.user._id
+      }).then((todo) => {
         if (!todo) {
           return res.status(404).send();
         }
@@ -54,14 +60,17 @@ app.get('/todos/:id', (req, res) => {
       });
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
       }
 
-    Todo.findByIdAndRemove(id).then((todo) => {
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         } 
@@ -73,31 +82,31 @@ app.delete('/todos/:id', (req, res) => {
 
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
-
+  
     if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+      return res.status(404).send();
     }
-
+  
     if (_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = new Date().getTime();
+      body.completedAt = new Date().getTime();
     } else {
-        body.completed = false;
-        body.completedAt =null;
+      body.completed = false;
+      body.completedAt = null;
     }
-
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-        if (!todo) {
-            return test.status(404).send();
-        }
-
-        res.send({todo});
+  
+    Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+  
+      res.send({todo});
     }).catch((e) => {
-        res.status(400).send();
+      res.status(400).send();
     })
-});
+  });
 
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
